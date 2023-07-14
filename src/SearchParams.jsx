2cdb0d4998f1c679051b772ac88ffc5a75efc121
11bus11 <02linfor01@gmail.com>
2023-07-14 21:20:03 +0200

    added hook for selection of breed
    made it possible to select what breed you are interested in

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,15 +1,32 @@
 import { useState } from 'react';
+const BREEDS = ["icelandic", "fjord", "arabic", "shetland"];
 
 const SearchParams = () => {
     const [location, setLocation] = useState("");
+    const [breed, setBreed] = useState("");
     return (
         <div className="search-params">
             <form>
                 <label htmlFor="location">
                 Location
                 <input onChange={e => setLocation(e.target.value)} id="location" value={location} placeholder="Location" />
-            </label>
-            <button>Submit</button>
+                </label>
+                <label htmlFor="breed">
+                    Breed
+                    <select
+                        id="breed"
+                        value={breed}
+                        onChange={(e) => {
+                            setBreed(e.target.value);
+                        }}
+                    >
+                        <option />
+                        {BREEDS.map(breed => (
+                            <option key={breed}>{breed}</option>
+                        ))}
+                    </select>
+                </label>
+                <button>Submit</button>
             </form>
         </div>
     )
