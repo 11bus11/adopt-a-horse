c0b5c899786f89f361b0b7f600d7639eb03713da
Erik Vodopivec Forsman <108095837+11bus11@users.noreply.github.com>
2023-08-05 22:16:32 +0200

    render pets only on submit
    Now the pets only render at the beginning and on submit.

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -11,7 +11,7 @@ const SearchParams = () => {
 
   useEffect(() => {
     requestPets();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, []);
 
   async function requestPets() {
     const res = await fetch(
@@ -24,7 +24,10 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form onSubmit={e => {
+        e.preventDefault();
+        requestPets();
+        }}>
         <label htmlFor="location">
           Location
           <input
