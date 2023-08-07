bcae8154d2213c93e619fbc8688b7acdd7355e17
Erik Vodopivec Forsman <108095837+11bus11@users.noreply.github.com>
2023-08-07 21:37:37 +0200

    finished hook for displaying breeds
    Finnished the custom hook for displaying breeds.

--- src/useBreedList.jsx
+++ src/useBreedList.jsx
@@ -21,7 +21,13 @@ export default function useBreedList(animal) {
 
             const res = await fetch(
                 `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-            );
+            )
+            const json =await res.json();
+            localCache[animal] = json.breeds || []
+            setBreedList(localCache[animal]);
+            setStatus("loaded");
         }
-    })
+    }, [animal]);
+
+    return [breedList, status];
 }
