94272872d9dee34217e9dcf30ccaf801f9cf74e0
Erik Vodopivec Forsman <108095837+11bus11@users.noreply.github.com>
2023-08-13 23:19:41 +0200

    added fetchPet (react querry)
    For getting details about pet

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Details from "./Details.jsx";
 import SearchParams from "./SearchParams.jsx";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-    
-      <h1>Adopt me!</h1>
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <h1>Adopt me!</h1>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,7 +1,10 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
 
 const Details = () => {
     const { id } = useParams();
+    const results = useQuery (["details", id], fetchPet);
     return <h2>{id}</h2>;
 };
 
