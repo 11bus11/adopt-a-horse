8e23d0ba57b351cb99f807b8f334360bc7e93e57
11bus11 <02linfor01@gmail.com>
2023-09-02 21:04:24 +0200

    created a static image carousel
    no selection of image shown yet

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -3,19 +3,19 @@ import { Component } from 'react';
 class Carousel extends Component {
     state = {
         active: 0
-    }
+    };
 
     static defaultProps = {
-        images: ["http://pets-images.dev-apis.com/pets/none.jpg"]
-    }
+        images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+    };
 
     render () {
-        const { active } = this.state
-        const { images } = this.props
+        const { active } = this.state;
+        const { images } = this.props;
 
         return (
-            <div className='carousel'>
-                <img src={images[active]} alt="animal hero" />
+            <div className="carousel">
+                <img src={images[active]} alt="animal" />
                 <div className='carousel-smaller'>
                     {images.map((photo, index) => (
                         <img
@@ -23,10 +23,12 @@ class Carousel extends Component {
                             src={photo}
                             className={index === active ? "active" : ""}
                             alt="animal thumbnail"
-                            />
+                        />
                     ))}
                 </div>
             </div>
         )
     }
 };
+
+export default Carousel;
